efab39639a8d21c137ac3087f0b7f0ebc1446af9
Alex Cui <cuizhihui030925@outlook.com>
2021-09-20 20:35:01 +0800

    把 Control 相关的 Block 原型移植过来了

--- src/block_definition.ts
+++ src/block_definition.ts
@@ -1,4 +1,3 @@
-import Block from "./block";
 import BlockPrototype, { BlockType, ParamType, ValueType } from "./block_prototype";
 
 const blockPrototypes = new Map<string, BlockPrototype>([
@@ -10,6 +9,50 @@ const blockPrototypes = new Map<string, BlockPrototype>([
     }],
 
     // Control
+    ['control_forever', {
+        opcode: 'control_forever',
+        type: BlockType.STATEMENT,
+        params: [
+            { name: 'SUBSTACK', type: ParamType.STATEMENT, valueType: ValueType.STATEMENT }
+        ],
+        toCode: (params: Map<string, string>): string => {
+            return [
+                `while (true) {`,
+                params.get('SUBSTACK'),
+                '}'
+            ].join('\n');
+        }
+    }],
+    ['control_repeat', {
+        opcode: 'control_repeat',
+        type: BlockType.STATEMENT,
+        params: [
+            { name: 'TIMES', type: ParamType.STATEMENT, valueType: ValueType.NUMBER },
+            { name: 'SUBSTACK', type: ParamType.STATEMENT, valueType: ValueType.STATEMENT }
+        ],
+        toCode: (params: Map<string, string>): string => {
+            return [
+                `for (int i = ${params.get('TIMES')}; i; --i) {`,
+                params.get('SUBSTACK'),
+                '}'
+            ].join('\n');
+        }
+    }],
+    ['control_if', {
+        opcode: 'control_if',
+        type: BlockType.STATEMENT,
+        params: [
+            { name: 'CONDITION', type: ParamType.INPUT, valueType: ValueType.ANY },
+            { name: 'SUBSTACK', type: ParamType.STATEMENT, valueType: ValueType.STATEMENT }
+        ],
+        toCode: (params: Map<string, string>): string => {
+            return [
+                `if (${params.get('CONDITION')}) {`,
+                params.get('SUBSTACK'),
+                '}'
+            ].join('\n');
+        }
+    }],
     ['control_if_else', {
         opcode: 'control_if_else',
         type: BlockType.STATEMENT,
@@ -28,19 +71,110 @@ const blockPrototypes = new Map<string, BlockPrototype>([
             ].join('\n');
         }
     }],
-    ['control_forever', {
-        opcode: 'control_forever',
-        type: BlockType.STATEMENT,
-        params: [
-            { name: 'SUBSTACK', type: ParamType.STATEMENT, valueType: ValueType.STATEMENT }
-        ],
-        toCode: (params: Map<string, string>): string => {return [
-            `while (true) {`,
-            params.get('SUBSTACK'),
-            '}'
-        ].join('\n');
+    ['control_stop', {
+        opcode: 'control_stop',
+        type: BlockType.BODY,
+        params: [],
+        toCode: (params: Map<string, string>): string => {
+            return 'return;';
         }
     }],
+    ['control_wait', {
+        opcode: 'control_wait',
+        type: BlockType.BODY,
+        params: [
+            { name: 'DURATION', type: ParamType.INPUT, valueType: ValueType.NUMBER }
+        ],
+        toCode: (params: Map<string, string>): string => {
+            return `wait(${params.get('DURATION')});`;
+        }
+    }],
+    ['control_wait_until', {
+        opcode: 'control_wait_until',
+        type: BlockType.STATEMENT,
+        params: [
+            { name: 'CONDITION', type: ParamType.INPUT, valueType: ValueType.ANY }
+        ],
+        toCode: (params: Map<string, string>): string => {
+            return `while (!${params.get('CONDITION')});`;
+        }
+    }],
+    ['control_repeat_until', {
+        opcode: 'control_repeat_until',
+        type: BlockType.STATEMENT,
+        params: [
+            { name: 'CONDITION', type: ParamType.INPUT, valueType: ValueType.ANY },
+            { name: 'SUBSTACK', type: ParamType.STATEMENT, valueType: ValueType.STATEMENT }
+        ],
+        toCode: (params: Map<string, string>): string => {
+            return [
+                `while (!${params.get('CONDITION')}) {`,
+                params.get('SUBSTACK'),
+                '}'
+            ].join('\n');
+        }
+    }],
+    ['control_while', { // deprecate
+        opcode: 'control_while',
+        type: BlockType.STATEMENT,
+        params: [
+            { name: 'CONDITION', type: ParamType.INPUT, valueType: ValueType.ANY },
+            { name: 'SUBSTACK', type: ParamType.STATEMENT, valueType: ValueType.STATEMENT }
+        ],
+        toCode: (params: Map<string, string>): string => {
+            return [
+                `while (${params.get('CONDITION')}) {`,
+                params.get('SUBSTACK'),
+                '}'
+            ].join('\n');
+        }
+    }],
+    ['control_for_each', { // deprecate
+        opcode: 'control_for_each',
+        type: BlockType.STATEMENT,
+        params: [
+            { name: 'VARIABLE', type: ParamType.FIELD, valueType: ValueType.FIELD },
+            { name: 'VALUE', type: ParamType.INPUT, valueType: ValueType.NUMBER },
+            { name: 'SUBSTACK', type: ParamType.STATEMENT, valueType: ValueType.STATEMENT }
+        ],
+        toCode: (params: Map<string, string>): string => {
+            return [
+                `for (${params.get('VARIABLE')} = 0; ${params.get('VARIABLE')} < ${params.get('VALUE')}; ++${params.get('VARIABLE')}) {`,
+                params.get('SUBSTACK'),
+                '}'
+            ].join('\n');
+        }
+    }],
+    ['control_start_as_clone', {
+        opcode: 'control_start_as_clone',
+        type: BlockType.HEAD,
+        params: []
+    }],
+    ['control_create_clone_of', {
+        opcode: 'control_create_clone_of',
+        type: BlockType.BODY,
+        params: [
+            { name: 'CLONE_OPTION', type: ParamType.FIELD, valueType: ValueType.FIELD }
+        ],
+        toCode: (params: Map<string, string>): string => {
+            return `this.clone(${params.get('CLONE_OPTION')});`;
+        }
+    }],
+    ['control_delete_this_clone', {
+        opcode: 'control_delete_this_clone',
+        type: BlockType.END,
+        params: [],
+        toCode: (params: Map<string, string>): string => {
+            return [
+                `this.deleteClone(${params.get('CLONE_OPTION')});`,
+                'return;'
+            ].join('\n');
+        }
+    }],
+    // control_get_counter // deprecate
+    // control_incr_counter // deprecate
+    // control_clear_counter // deprecate
+    // control_all_at_once // deprecate
 
     // Operator
     ['operator_add', {
